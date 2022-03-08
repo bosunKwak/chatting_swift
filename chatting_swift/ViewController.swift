710bea412f27c5305f5845e91ec1f1f94175a10a
Bosun Kwak <214kbs@gmail.com>
2022-03-08 10:39:51 +0900

    Initial Commit

---
+++ chatting_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatting_swift
+//
+//  Created by 곽보선 on 2022/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
